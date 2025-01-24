0630c4dee4d7a9d2626ec6d823a68d6417b1a6d4
RudraDevan G <105837233+RudraDevaDev@users.noreply.github.com>
2025-01-24 20:16:14 +0530

    Create Switch.R

---
+++ Switch.R
@@ -0,0 +1,3 @@
+month <- as.integer(readline(“Enter month number:”))
+season <- switch(month,"Winter","Spring","Spring","Summer","Summer"," Summer ","Summer","Rainy","Rainy","Rainy","Winter","Winter")
+print(season)
